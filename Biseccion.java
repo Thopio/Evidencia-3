392a055b222b796ae10ae3bcfbc2a07dae34d978
Thopio <jesusolupio@gmail.com>
2025-05-15 21:33:26 -0700

    Actualizar archivo Biseccion.java casi terminado

--- Biseccion.java
+++ Biseccion.java
@@ -23,5 +23,38 @@ public class Biseccion {
         double xrAnterior = 0;
         double error = 100; //error inicial
         int iteracion = 1;
+
+        System.out.println("\nIteración\t xl\t\t xu\t\t Xr\t\t f(xl)\t\t f(xu)\t\t f(xl)*f(xu)\t Error (%)");
+
+        do {
+            double fxl = funcion(xl);
+            double fxu = funcion(xu);
+            double producto = fxl * fxu;
+
+            xrAnterior = xr;
+            xr = (xl + xu) / 2; //Calcular el nuevo Xr
+
+            //Operacion para decidir qué rango cambiar
+            if (producto > 0) {
+                xu = xr; //Cambiar el rango mayor
+            } else if (producto < 0) {
+                xl = xr; //Cambiar el rango menor
+            } else {
+                //Producto == 0 implica que es raiz exacta
+                break;
+            }
+
+            //Calcular error a partir de la segunda iteración
+            if (iteracion > 1) {
+                error = Math.abs((xr - xrAnterior) / xr) * 100;
+            }
+
+            //Mostrar datos de la iteración
+            System.out.printf("%d\t\t %.6f\t %.6f\t %.6f\t %.6f\t %.6f\t %.6f\t %.4f\n",
+                    iteracion, xl, xu, xr, fxl, fxu, producto, error);
+
+            iteracion++;
+
+        } while (error > 1 && iteracion <= 100); //Detener si error < 1% o se exceden 100 iteraciones
     }
 }
